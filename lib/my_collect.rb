995f6d72547900b15d5c9e1a5ec06160a4f88cc2
Robert Quintana <robertquintanatx@gmail.com>
2020-05-29 04:37:11 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ def my_collect(name_array)
   i = 0 
   mod_array = []
   while i < name_array.length 
-    mod_array << yield(name_array[i])
+    mod_array << yield name_array[i]
     i += 1 
   end
 end 
